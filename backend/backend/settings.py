d32cf21d6be680ce98485501b6625c0700ca7603
Julius Breindl <jbreindl@buffalo.edu>
2021-09-25 20:28:27 -0400

    #14: set up Postgres for django (currently running on localhost)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ProductivityApp',
+        'USER': os.getenv('UNAME'),
+        'PASSWORD':os.getenv('PASSWD'),
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 

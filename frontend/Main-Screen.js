5f0f3d8700873ba8e15248bbcaeaa8c2daec57bb
Demeyer10 <demeyer@buffalo.edu>
2021-10-03 14:39:33 -0400

    Update Main-Screen.js

--- frontend/Main-Screen.js
+++ frontend/Main-Screen.js
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
-import back from './Images/Background.png'
+import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, TextInput, Touchable,Image, requireNativeComponent} from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState('Homework');
   return (
     <><View style={styles.MainScreen}>
    
@@ -22,75 +22,144 @@ export default function App() {
 
        
 
-        <View opacity = {0.2} style = {{ 
-          alignItems: "right", 
-          marginTop: '-10%',  
+        <View opacity = {0.3} style = {{ 
+          alignItems: "right",  
           width: '80%',
           height: '5%',
           frontSize: 20,
           backgroundColor: '#457890',
           borderColor: 'black',
-          borderWidth: 2}}
+          borderWidth: 2,
+          left: '10%',
+          borderRadius: '20%',
+          shadowOpacity: 20}}
         >
           <TextInput sytle = {styles.input} 
-            placeholder = "Enter Task here"
+            placeholder = "Create new task"
             placeholderTextColor = 'white'
-            top = {5}
+            top = {7}
+            width = {'100%'}
+            color = 'white'
             frontSize = {20}
-          />
-        </View>
-       
-        <Text style = {{fontSize: 40, marginTop: '-25%', color: '#457890', right: 80}}>Today</Text>
-        <View style={{height: 300, width: 300, backgroundColor: 'transparent', top: '20%'}}>
+            left = {10}
+            onChangeText = {(text) => setTask(text)}/>
         </View>
         
+       
+        <Text style = {{fontSize: 40, color: '#457890', left: '10%', bottom: '13%'}}>Today</Text>
+        <View style = {styles.tagHeader}>
+          <Text style = {{fontSize: 20, position: 'relative'}}>Homework</Text>
+          <Text style = {{fontSize: 15}}>1</Text>
+        </View>
+
+        <View style ={ styles.taskBox}>
+          <View style = {styles.mainTaskHeader}> 
+            <TouchableOpacity style = {styles.checkCircle}/>
+            <Text style = {{fontSize: 18 ,color:  '#457890', left: 15, top: 4}}>Math Homework</Text>
+          </View>
+          <View style = {styles.subtaskHeader}> 
+            <TouchableOpacity style = {styles.checkCircle}/>
+            <Text style = {{fontSize: 18 ,color:  '#457890',left: 15, top: 4}}>Part A</Text>
+          </View>
+          <View style = {styles.subtaskHeader}>
+            <TouchableOpacity style = {styles.checkCircle}/>
+            <Text style = {{fontSize: 18 ,color:  '#457890',left: 15, top: 4}}>Part B</Text>
+          </View>
+          <View style = {styles.subtaskHeader}>
+            <TouchableOpacity style = {styles.checkCircle}/>
+            <Text style = {{fontSize: 18 ,color:  '#457890',left: 15, top: 4}}>Part C</Text>
+          </View>
+          <View style = {styles.subtaskHeader}>
+            <TouchableOpacity style = {styles.checkCircle}/>
+            <Text style = {{fontSize: 18 ,color:  '#457890',left: 15, top: 4}}>Part E</Text>
+          </View>
+        </View>
         </View>
       </>
       
+      
     );
 }
+
+
 
 const styles = StyleSheet.create({
   MainScreen: {
     flex: 1,
     backgroundColor: '#FAEBEF',
-    alignItems: 'center'
+    alignItems: 'baseline',
+    width: '100%',
+    height: '100%'
   },
   TaskBarContainer:{
-    bottom: '-155%',
     flexDirection: 'row',
     backgroundColor: 'transparent',
     width: '100%',
     height: '10%',
     justifyContent: 'space-evenly',
+    position: 'absolute',
+    bottom: 30
   },
   CricleOverlay:{
-    top: '10%',
-    width: 75,
-    height: 75,
-    borderRadius: 75/2,
+    width: '20%',
+    height: '90%',
+    borderRadius: 500000/2,
     backgroundColor: '#FAEBEF',
     alignItems: 'center',
   },
   innerCircle:{
     top: '10%',
-    width: 60,
-    height: 60,
-    borderRadius: 60/2,
+    width: '80%',
+    height: '80%',
+    borderRadius: 500000/2,
     backgroundColor: '#457890',
+    shadowOpacity: .5,
   },
   input: {
     margin: 15,
     height: 40,
     borderColor: 'black',
-    borderWidth: 1
+    borderWidth: 1,
+    left: 20,
+    color: 'white'
   },
   imagestyle:{
     top: '10%',
     width: 60,
     height: 60,
     borderRadius: 60/2,
+  },
+  checkCircle:{
+    width: 25,
+    height: 25,
+    borderRadius: 500/2,
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    top: 2,
+    left: 9
+  },
+  tagHeader: {color: '#457890', 
+  left: '5%', 
+  flexDirection: 'row'
+},
+  taskBox: {
+    flexGrow: 0,
+    width: '80%',
+    backgroundColor: '#A8DADC', 
+    left: '10%', borderRadius: 25, 
+    flexDirection: 'column'
+  },
+  mainTaskHeader:{
+    flexGrow: 0,
+    flexDirection: 'row', 
+    height: 50, 
+    width: '100%'
+  },
+  subtaskHeader:{
+    flexDirection: 'row', 
+    height: 50, 
+    width: '100%', 
+    left: 30
   }
-
 
 });

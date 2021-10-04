82da9e6945d4d1b403aa0d9d3cc271aaedcee629
l.simpson <l.simpson@wustl.edu>
2021-10-04 11:43:26 -0400

    #22: Task Object created

--- backend/quickstart/models.py
+++ backend/quickstart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    creation_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    completion_date = models.DateField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{} - {}".format(self.title,self.due_date,self.completed)

06823e693344465797cfab1f34ce6bae3c00a021
Demeyer10 <demeyer@buffalo.edu>
2021-10-07 13:20:27 -0400

    #32: Creation of register page

---
+++ frontend/Register-Page.js
@@ -0,0 +1,13 @@
+import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, TextInput, Touchable,Image, requireNativeComponent} from 'react-native';
+
+export default function App() {
+  return (
+      <View> </View>
+    );
+}
+
+const styles = StyleSheet.create ({
+    
+})

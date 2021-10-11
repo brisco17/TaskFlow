265b404fb76dc6b094ab3771787e0947432a8115
Julius Breindl <jbreindl@buffalo.edu>
2021-10-10 23:33:06 -0400

    #33: Defined Tag model and serializer

---
+++ backend/tag/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from rest_framework import serializers
+
+class Tag(models.Model):
+    # allow users to define a title and description for a tag
+    title = models.CharField(unique=True)
+    description = models.CharField()
+
+    # associate tags with a user
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE, # on delete all of this users tags will be removed
+    )
+    
+class CommentSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    description = serializers.CharField()
+
+

8d712381ce78a69d55950fe401fc11c1d781eb2d
l.simpson <l.simpson@wustl.edu>
2021-10-17 17:53:19 -0400

    #37: PUT and DELETE implementation

--- backend/task/views.py
+++ backend/task/views.py
@@ -1,13 +1,21 @@
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework import generics
+#from rest_framework import permissions
+
 
 
 class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]

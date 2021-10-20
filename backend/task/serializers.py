19860d5c7b984d36359b79c720365b69ca3630f5
l.simpson <l.simpson@wustl.edu>
2021-10-19 23:31:04 -0400

    #35: Needs Userprofile urls

--- backend/task/serializers.py
+++ backend/task/serializers.py
@@ -4,6 +4,6 @@ from rest_framework import serializers
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
-        fields = ['id','title','description','creation_date','due_date','completion_date','completed']
-        #owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['id','owner','title','description','creation_date','due_date','completion_date','completed']
+        owner = serializers.ReadOnlyField(source='owner.username')
 
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,21 +1,21 @@
 from .models import Task
 from .serializers import TaskSerializer
-from rest_framework import generics
-#from rest_framework import permissions
+from rest_framework import generics, permissions
+from .permissions import IsOwner
 
 
 
 class TaskList(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-    #def perform_create(self, serializer):
-    #    serializer.save(owner=self.request.user)
+    permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        queryset = Task.objects.all().filter(owner=self.request.user)
+        return queryset
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated,IsOwner]

0d72472fc02f0dad9bf0a2571ecf7cf681f22dc3
Julius Breindl <jbreindl@buffalo.edu>
2021-10-20 14:56:31 -0400

    #46: Updated the tags views to have protections read/write protections

--- backend/tag/views.py
+++ backend/tag/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from backend.settings.permissions import IsOwner
 from rest_framework.parsers import JSONParser
 from .models import Tag
 from .serializers import TagSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class TagList(generics.ListCreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwner] 
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
 
 class TagDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwner] 

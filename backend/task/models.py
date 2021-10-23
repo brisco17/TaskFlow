f5279871cb64fafcf1396d6d9b4de280ae42d9e2
l.simpson <l.simpson@wustl.edu>
2021-10-22 20:28:44 -0400

    #35: Tasks only viewable by their owner

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,21 +1,26 @@
 from django.db import models
-
 from django.conf import settings
 
 # Create your models here.
 
 class Task(models.Model):
-    owner = models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name = 'task',
-        on_delete=models.CASCADE,
-        default=''
-        )
+        on_delete=models.CASCADE
+    )
+    tag = models.ForeignKey(
+        'tag.Tag',
+        on_delete=models.SET_NULL,
+        blank = True,
+        related_name='task',
+        null = True
+    )
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     creation_date = models.DateField(auto_now_add=True)
     due_date = models.DateField()
-    completion_date = models.DateField(blank=True)
+    completion_date = models.DateField(blank=True,null=True)
     completed = models.BooleanField(default=False)
 
     def __str__(self):

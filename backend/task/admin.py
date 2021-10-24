0060349bbd0624d3c641a7c3b2762bdf44fb73fd
Julius Breindl <jbreindl@buffalo.edu>
2021-10-24 18:23:07 -0400

    #46: accidental changes made to these files, reverted

--- backend/task/admin.py
+++ backend/task/admin.py
@@ -5,6 +5,6 @@ from .models import Task
 # Register your models here.
 
 class taskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'due_date', 'completed','user')
+    list_display = ('id', 'title', 'due_date', 'completed',)
 
 admin.site.register(Task, taskAdmin)

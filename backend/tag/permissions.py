221decd0878eb0cdd636f4bc7b7019a3f8180601
Julius Breindl <jbreindl@buffalo.edu>
2021-10-24 17:12:22 -0400

    #46: Permissions fixed

--- backend/tag/permissions.py
+++ backend/tag/permissions.py
@@ -9,4 +9,4 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
 
         # Write permissions are only allowed to the owner of the snippet.
-        return obj.user == request.user
+        return self.has_permission(request, view) and obj.user == request.user

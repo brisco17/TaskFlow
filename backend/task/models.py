ad9b5d63854e2931896d321710f5dede45c302a2
Julius Breindl <jbreindl@buffalo.edu>
2021-10-24 20:12:08 -0400

    #46: this file also got reverted accidentally.

--- backend/task/models.py
+++ backend/task/models.py
@@ -4,12 +4,7 @@ from django.conf import settings
 # Create your models here.
 
 class Task(models.Model):
-    """
-    owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        )
-    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="task", on_delete=models.CASCADE,blank=True, null=True)
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     creation_date = models.DateField(auto_now_add=True)

34e16a1420d4331f70bf42a1b1f817f5bbb1517b
Julius Breindl <jbreindl@buffalo.edu>
2021-10-24 20:40:59 -0400

    #46: more things got deleted here

--- backend/task/models.py
+++ backend/task/models.py
@@ -5,6 +5,13 @@ from django.conf import settings
 
 class Task(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="task", on_delete=models.CASCADE,blank=True, null=True)
+    tag = models.ForeignKey(
+        'tag.Tag',
+        on_delete=models.SET_NULL,
+        blank = True,
+        related_name='task',
+        null = True
+    )
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     creation_date = models.DateField(auto_now_add=True)
